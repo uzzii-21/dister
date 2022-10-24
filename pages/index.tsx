eb2b2f6965e3a1875b111e84d00c373155e843e3
Uzair Ahmed <uzairahmed74372@gmail.com>
2022-10-24 18:44:44 +0500

    update __next css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      {/* sidebar */}
+      <aside>Sidebar</aside>
+      <main>
+        <h1>Home</h1>
+      </main>
+      <div>PlayTracker</div>
+    </>
+  );
 };
 
 export default Home;
